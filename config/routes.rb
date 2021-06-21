d9d68f432eb62cf8695664c7515192272df1a308
James <james.ryan.mcguire@gmail.com>
2021-06-21 10:32:19 -0700

    Adds query, segement and body param path routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
   get "/all_movies", controller: "movies", action: "all_movies"
 
   get "/first_movie", controller: "movies", action: "first_movie"
+
+  get "/query_param", controller: "actors", action: "display_actor"
+
+  get "segment_param/:id", controller: "actors", action: "display_actor"
+
+  post "body_param", controller: "actors", action: "display_actor"
 end
