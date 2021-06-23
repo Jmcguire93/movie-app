e1e97782644049da5ba7228215a164511fabbefc
James <james.ryan.mcguire@gmail.com>
2021-06-23 10:20:49 -0700

    Adds index, show and create routes and controller methods for actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,22 +1,39 @@
 class ActorsController < ApplicationController
 
-  def first_actor
-    actor = Actor.first
+  def index
+    actors = Actor.all 
+    render json: actors.as_json 
+  end
+
+  def show
+    actor_id = params["id"]
+    actor = Actor.find_by(id: actor_id)
     render json: actor.as_json
   end
 
-  def last_actor
-    last_actor = Actor.last_actor
-    render json: last_actor.as_json
-  end
+  def create
+    actor = Actor.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"]
+    )
 
-  def all_actors
-    all_actors = Actor.all 
-    render json: all_actors.as_json 
-  end
-
-  def display_actor
-    actor = Actor.find(params[:id])
+    actor.save
     render json: actor.as_json
   end
+  
+  
+  
+  
+  
+  # def last_actor
+  #   last_actor = Actor.last_actor
+  #   render json: last_actor.as_json
+  # end
+
+
+  # def display_actor
+  #   actor = Actor.find(params[:id])
+  #   render json: actor.as_json
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
   
-  get "/first_actor", controller: "actors", action: "first_actor"
+  get "/actors", controller: "actors", action: "index"
+  
+  get "/actors/:id", controller: "actors", action: "show"
 
-  get "/last_actor", controller: "actors", action: "last_actor"
-
-  get "/all_actors", controller: "actors", action: "all_actors"
-
-  get "/all_movies", controller: "movies", action: "all_movies"
-
-  get "/first_movie", controller: "movies", action: "first_movie"
-
-  get "/query_param", controller: "actors", action: "display_actor"
-
-  get "segment_param/:id", controller: "actors", action: "display_actor"
-
-  post "body_param", controller: "actors", action: "display_actor"
+  post "actors", controller: "actors", action: "create"
+  
+  
+  
+  
+  
+  
+  
+  # get "/last_actor", controller: "actors", action: "last_actor"
+  
+  # get "/all_movies", controller: "movies", action: "all_movies"
+  
+  # get "/first_movie", controller: "movies", action: "first_movie"
+  
+  # get "/query_param", controller: "actors", action: "display_actor"
+  
+  # get "segment_param/:id", controller: "actors", action: "display_actor"
+  
+  # post "body_param", controller: "actors", action: "display_actor"
 end
