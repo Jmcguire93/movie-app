99ecf92cc67189e8c50fdfdfb42c978b75b24b1c
James <james.ryan.mcguire@gmail.com>
2021-06-23 10:33:18 -0700

    Adds restful routes and controller methods for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,65 @@
 class MoviesController < ApplicationController
-  def all_movies
-    all_movies = Movie.all 
-    render json: all_movies.as_json
+  
+  def index
+    movies = Movie.all 
+    render json: movies.as_json 
   end
 
-  def first_movie
-    first_movie = Movie.first 
-    render json: first_movie.as_json
+  def show
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+    render json: movie.as_json
   end
+
+  def create
+    movie = Movie.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"]
+    )
+
+    movie.save
+    render json: movie.as_json
+  end
+  
+  def update 
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+
+    movie.first_name = params["first_name"] || movie.first_name
+    movie.last_name = params["last_name"] || movie.last_name
+    movie.known_for = params["known_for"] || movie.known_for 
+
+    movie.save
+    render json: movie.as_json
+  end
+  
+  def destroy
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+    movie.destroy
+
+    render json: {message: "Movie sucesfully canceled." }
+  end   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # def all_movies
+  #   all_movies = Movie.all 
+  #   render json: all_movies.as_json
+  # end
+
+  # def first_movie
+  #   first_movie = Movie.first 
+  #   render json: first_movie.as_json
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,17 @@ Rails.application.routes.draw do
   patch "/actors/:id", controller: "actors", action: "update"
 
   delete "actors/:id", controller: "actors", action: "destroy"
+
+
+  get "/movies", controller: "movies", action: "index"
+  
+  get "/movies/:id", controller: "movies", action: "show"
+
+  post "movies", controller: "movies", action: "create"
+
+  patch "/movies/:id", controller: "movies", action: "update"
+
+  delete "movies/:id", controller: "movies", action: "destroy"
   
   
   
