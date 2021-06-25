5ea927d30c7081b51cc1977017b60174641e3407
James <james.ryan.mcguire@gmail.com>
2021-06-25 11:18:23 -0700

    Adds validations for actor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_173817) do
+ActiveRecord::Schema.define(version: 2021_06_24_174344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_06_24_173817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "gender"
+    t.integer "age"
+    t.integer "views"
   end
 
   create_table "movies", force: :cascade do |t|
