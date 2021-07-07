45ad33f40daa11de4ab2312fa2468e79798ad814
James <james.ryan.mcguire@gmail.com>
2021-07-06 21:36:54 -0700

    Updates movie serialzier by adding genre attribute

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :plot, :director, :english, default: :true
+  attributes :id, :title, :year, :plot, :director, :english, default: :true, :genres
 
   has_many :actors
 end
